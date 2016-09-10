6b581e921d5ccd7836b2e2d8595f1059ea99b31a
Akintunde Andrews <Akintunde Andrews>
2016-09-09 20:09:29 -0400

    Initial Commit
    n/a

---
+++ Welcome1.java
@@ -0,0 +1,11 @@
+//This is class welcome 
+
+public class Welcome1
+{
+	public static void main(String[] args)
+	{
+		//Display a welcome message on console
+		System.out.printf("%s%n%s", "Welcome to ", "Java Programming");
+		
+	}
+}
